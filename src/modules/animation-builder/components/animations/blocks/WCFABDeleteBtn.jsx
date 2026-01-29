d353eeb57169c4320acd87bddafff670c1eecb32
Sabbir Ahamed Maruf <shopify.dev.wc@gmail.com>
2026-01-29 10:10:59 +0600

    update delete btn icon for test

--- src/modules/animation-builder/components/animations/blocks/WCFABDeleteBtn.jsx
+++ src/modules/animation-builder/components/animations/blocks/WCFABDeleteBtn.jsx
@@ -3,7 +3,7 @@ import { HugeiconsIcon } from "@hugeicons/react";
 import { Delete01Icon } from "@hugeicons/core-free-icons";
 import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
-import { MinusSignIcon } from "@hugeicons/core-free-icons/index";
+import { Cancel01Icon, MinusSignIcon } from "@hugeicons/core-free-icons/index";
 
 const deleteBtnVariants = cva(
   "bg-transparent text-foreground-secondary hover:text-button-destructive-hover hover:scale-105 border-none outline-none rounded-5 transition-colors duration-200 ease-in-out cursor-pointer",
@@ -28,7 +28,8 @@ const WCFABDeleteBtn = ({ size = "", onDelete = () => {} }) => {
       className={cn(deleteBtnVariants({ size }))}
     >
       {/* <HugeiconsIcon icon={Delete01Icon} size={12} strokeWidth={2} /> */}
-      <HugeiconsIcon icon={MinusSignIcon} size={12} strokeWidth={2} />
+      {/* <HugeiconsIcon icon={MinusSignIcon} size={12} strokeWidth={2} /> */}
+      <HugeiconsIcon icon={Cancel01Icon} size={12} strokeWidth={2} />
     </Button>
   );
 };
