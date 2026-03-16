8353a2622bc1776d60c224691df38071ed330932
Rayhan Uddin <rayhanuddin.it.2019@gmail.com>
2026-03-16 16:05:04 +0600

    vers

--- gsap-animation-builder-for-wordpress.php
+++ gsap-animation-builder-for-wordpress.php
@@ -3,7 +3,7 @@
  * Plugin Name: Motionkit Connector for WordPress
  * Plugin URI: https://github.com/your-username/motionkit
  * Description: A WordPress plugin that integrates the GSAP Animation Builder (Motionkit) to create and manage animations directly from the WordPress admin dashboard.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: wealcoder
  * Author URI: https://profiles.wordpress.org/your-username/
  * License: GPL v2 or later
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('MOTIONKIT_VERSION', '1.0.0');
+define('MOTIONKIT_VERSION', '1.0.1');
 define('MOTIONKIT_PLUGIN_FILE', __FILE__);
 define('MOTIONKIT_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('MOTIONKIT_PLUGIN_URL', plugin_dir_url(__FILE__));
