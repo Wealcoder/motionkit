994dc8d40e2be96e9e7adaa27e55da530e063745
Sabbir Ahamed Maruf <shopify.dev.wc@gmail.com>
2026-04-13 12:50:50 +0600

    Fix (Allowed Origin): Added allowed origin for linux

--- includes/RestApi/RestApi.php
+++ includes/RestApi/RestApi.php
@@ -70,8 +70,11 @@ final class RestApi
     $allowed_origins = apply_filters('motionkit/editor/allowed_origins', [
       'https://editor.motionkit.io',
       'http://localhost:5173',
+      'http://127.0.0.1:5173',
       'http://localhost:5174',
+      'http://127.0.0.1:5174',
       'http://localhost:3000',
+      '*'
     ]);
 
     if (!empty($origin) && in_array($origin, $allowed_origins, true)) {
--- src/modules/animation-builder/editor-bridge.js
+++ src/modules/animation-builder/editor-bridge.js
@@ -21,8 +21,11 @@ import { handleMediaQuery } from "@/lib/utils";
 const ALLOWED_ORIGINS = [
   "https://editor.motionkit.io",
   "http://localhost:5173",
+  "http://127.0.0.1:5173",
   "http://localhost:5174",
+  "http://127.0.0.1:5174",
   "http://localhost:3000",
+  "*",
 ];
 
 /**
@@ -316,7 +319,7 @@ function receivePageConfig() {
             },
           );
         }
-       
+
         document.dispatchEvent(
           new CustomEvent("motionkit-settings-update", {
             detail: {
