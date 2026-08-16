ca45c1953d9372c433407ff69135d99f70b23f19
Sabbir Ahamed Maruf <shopify.dev.wc@gmail.com>
2026-08-16 17:30:05 +0600

    Release : (MotionKit Connector): 0.5.1

--- motionkit.php
+++ motionkit.php
@@ -4,7 +4,7 @@
  * Plugin Name: Motionkit – Visual Animation with GSAP for WordPress
  * Plugin URI: https://motionkit.io
  * Description: Connects your site to the Motionkit visual editor so you can build GSAP-powered scroll, hover, and page-transition animations without writing code.
- * Version: 0.5.0
+ * Version: 0.5.1
  * Author: wealcoder
  * Author URI: https://profiles.wordpress.org/wealcoder/
  * License: GPL v2 or later
